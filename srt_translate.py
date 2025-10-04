ea22ebbef5b7cf13637ca1f08da5ee42637afe33
vfgchen <vfgchen@163.com>
2025-10-04 19:47:47 +0800

    fix

--- srt_translate.py
+++ srt_translate.py
@@ -7,20 +7,27 @@ import asyncio
 
 srt_dir = "subtitles"
 out_dir = "output"
-if os.path.exists(out_dir):
+if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
-for srt_file in glob.glob(os.path.join(srt_dir, "**/*zh.srt"), recursive=True):
-    basename = os.path.basename(srt_file).split("-")[0]
-    out_file = f"{out_dir}/{basename}-zh.mp3"
+def main():
+    for srt_file in glob.glob(os.path.join(srt_dir, "**/*zh.srt"), recursive=True):
+        basename = os.path.basename(srt_file).split("-")[0]
+        out_file = f"{out_dir}/{basename}-zh.mp3"
 
-    print(f"generate mp3: {out_file}")
-    asyncio.get_event_loop().run_until_complete(_main(
-        srt_data = pysrt.open(srt_file),
-        voice = "zh-CN-XiaoxiaoNeural",
-        out_file = out_file,
-        rate = "+0%",
-        volume = "+0%",
-        batch_size = 50,
-        enhanced_srt = False,
-    ))
+        try:
+            asyncio.get_event_loop().run_until_complete(_main(
+                srt_data = pysrt.open(srt_file),
+                voice = "zh-CN-XiaoxiaoNeural",
+                out_file = out_file,
+                rate = "+0%",
+                volume = "+0%",
+                batch_size = 50,
+                enhanced_srt = False,
+            ))
+        finally:
+            print(f"generate mp3: {out_file}")
+
+
+if __name__ == "__main__":
+    main()
