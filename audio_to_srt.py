4ac43e93d52a8b049f943676370b3077f29f4652
vfgchen <vfgchen@163.com>
2025-10-05 23:51:04 +0800

    audio to srt

--- audio_to_srt.py
+++ audio_to_srt.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="audio to srt")
     parser.add_argument("audio_dir", help="audio dir", default="audios")
     parser.add_argument("--srt_dir", help="srt dir", default="subtitles")
-    parser.add_argument("--audio_type", help="audio type", default="mp3")
+    parser.add_argument("--audio_type", help="audio type", default="wav")
     parser.add_argument("--lang", help="lang", default="en")
     args = parser.parse_args()
 
