25a4d719dd55354e2f2aa78c96d80ba2525f0daa
vfgchen <vfgchen@163.com>
2025-10-10 21:51:26 +0800

    srt to txt

--- srt_to_txt.py
+++ srt_to_txt.py
@@ -40,30 +40,6 @@ def srt_to_txt(
     print(f"srt_to_txt: {srt_file} -> {txt_file}")
     return txt_file
 
-# txt to srt
-def txt_to_srt(
-        txt_file,
-        srt_file,
-        ref_srt_file,
-    ):
-    # 创建 srt_file 所在目录
-    if not path.exists(path.dirname(srt_file)):
-        os.makedirs(path.dirname(srt_file))
-    # txt 行
-    with open(txt_file, 'r', encoding="utf-8") as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines if len(line.strip()) > 0]
-    # ref_srt_file 行
-    ref_subs = pysrt.open(ref_srt_file)
-    for index, ref_sub in enumerate(ref_subs, start=1):
-        ref_sub.index = index
-        ref_sub.text = lines[index - 1]
-    # 创建字幕文件
-    srt_subs = pysrt.SubRipFile(ref_subs)
-    srt_subs.save(srt_file, encoding="utf-8")
-    print(f"txt_to_srt: {txt_file} -> {srt_file}")
-    return srt_file
-
 def srts_correct(
         srt_dir,
         suffix=".srt",
@@ -79,49 +55,13 @@ def srts_to_txts(
         txt_file = f"{path.splitext(srt_file)[0]}.txt"
         srt_to_txt(srt_file, txt_file)
 
-def txts_to_srts(
-        txt_dir,
-        suffix="zh.txt",
-        ref_srt_lang="en"
-    ):
-    for txt_file in glob.glob(path.join(txt_dir, f"**/*{suffix}"), recursive=True):
-        basename = path.basename(txt_file).split("-")[0]
-        srt_file = f"{path.splitext(txt_file)[0]}.srt"
-        ref_srt_file = path.join(txt_dir, f"{basename}-{ref_srt_lang}.srt")
-        print(ref_srt_file)
-        assert path.exists(ref_srt_file)
-        txt_to_srt(
-            txt_file=txt_file,
-            srt_file=srt_file,
-            ref_srt_file=ref_srt_file
-        )
-
 if __name__ == "__main__":
-    #-------------------
-    # root_dir = "E:/Download/D365FO_Training/Udemy_PL200_Microsoft Power Platform Functional Consultant 2025-5"
-    # en_srt_dir = path.join(root_dir, "en-srt")
-    # srts_correct(en_srt_dir)
-    # srts_to_txts(en_srt_dir)
-    
-    #-------------------
-    srt_file="D:/output/001-en.srt"
-    txt_file="D:/output/001-en.txt"
-    srt_to_txt(srt_file, txt_file)
+    parser = argparse.ArgumentParser(description="srt to txt")
+    parser.add_argument("--srt_dir", help="srt dir", default="subtitles")
+    parser.add_argument("--suffix", help="filename suffix", default="en.srt")
+    args = parser.parse_args()
 
-    #-------------------
-    # txt_file="D:/output/001-zh.txt"
-    # srt_file="D:/output/001-zh.srt"
-    # ref_srt_file="D:/output/001-en.srt"
-    # txt_to_srt(txt_file, srt_file, ref_srt_file)
-
-    #-------------------
-    # srt_dir="D:/output"
-    # srts_correct(srt_dir)
-
-    #-------------------
-    # txt_dir="D:/output"
-    # srts_to_txts(txt_dir)
-
-    #-------------------
-    # txt_dir="D:/output"
-    # txts_to_srts(txt_dir)
+    srts_to_txts(
+        srt_dir = args.srt_dir,
+        suffix  = args.suffix,
+    )
