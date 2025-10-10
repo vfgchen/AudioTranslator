7f00129a29291ecb8eb31e6b95042fd6e36c313d
vfgchen <vfgchen@163.com>
2025-10-11 01:56:49 +0800

    aitxt check

--- test_check_aitxt.py
+++ test_check_aitxt.py
@@ -16,7 +16,6 @@ def check_aitxt(
     basename = f"{path.splitext(aitxt_file)[0]}"
     ref_lines = []
     ref_file = f"{basename}.txt"
-    print(ref_file)
     if path.exists(ref_file):
         with open(ref_file, "r", encoding="utf-8") as file:
             ref_lines = [line.strip() for line in file if len(line.strip()) > 0]
@@ -26,7 +25,9 @@ def check_aitxt(
         ref_lines = [sub for sub in subs if len(sub.text.strip()) > 0]
     
     # 比对两者的行数
-    return len(lines) == len(ref_lines)
+    is_match = len(lines) == len(ref_lines)
+    report = f"txt: {aitxt_file}, ref: {ref_file}, txt_len: {len(lines)}, ref_len: {len(ref_lines)}"
+    return is_match, report
 
 def check_aitxts(
         aitxt_dir="subtitles",
@@ -34,8 +35,9 @@ def check_aitxts(
     ):
     result = []
     for aitxt_file in glob.glob(path.join(aitxt_dir, f"**/*{suffix}"), recursive=True):
-        if not check_aitxt(aitxt_file=aitxt_file):
-            result.append(aitxt_file)
+        is_match, report = check_aitxt(aitxt_file=aitxt_file)
+        if not is_match:
+            result.append(report)
     return result
 
 if __name__ == "__main__":
@@ -48,8 +50,8 @@ if __name__ == "__main__":
         aitxt_dir   = args.aitxt_dir,
         suffix      = args.suffix,
         )
-    filenames = "\n".join([item for item in result])
-    print(filenames)
+    reports = "\n".join([item for item in result])
+    print(reports)
     diff_file = path.join(args.aitxt_dir, "diff.txt")
     with open(diff_file, "w", encoding="utf-8") as file:
-        file.write(filenames)
+        file.write(reports)
