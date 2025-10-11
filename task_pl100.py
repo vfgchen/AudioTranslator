e6d115f619ac24ce7ed26a8c0eb15a83b7d4e948
vfgchen <vfgchen@163.com>
2025-10-11 19:02:33 +0800

    task_pl100

---
+++ task_pl100.py
@@ -0,0 +1,23 @@
+from os import path
+from deepseek_txt_to_aitxt import txt_ai_translate
+from deepseek_txt_to_aitxt import build_client
+import argparse
+
+root_dir = "D:/output/subtitles"
+def resolve(item): return path.join(root_dir, item)
+
+parser = argparse.ArgumentParser(description="audio to srt")
+parser.add_argument("--api_key", help="app_key", default="")
+args = parser.parse_args()
+
+# 读取任务
+with open(resolve("task.txt"), "r", encoding="utf-8") as file:
+    tasks = file.readlines()
+chat_client = build_client(api_key=args.api_key)
+for task in tasks:
+    txt_file = resolve(task)
+    content_file, reasoning_file = txt_ai_translate(
+        txt_file=txt_file,
+        chat_client=chat_client,
+        topic="Power Platform: PL-100",
+    )
