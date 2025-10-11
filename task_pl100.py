c3751c6d3835f437f1c8c1023408e4308c9af78a
vfgchen <vfgchen@163.com>
2025-10-12 01:27:18 +0800

    async batch

--- task_pl100.py
+++ task_pl100.py
@@ -5,6 +5,11 @@ import argparse
 import asyncio
 import os
 
+# 分组生成器
+def batch_generator(data, batch_size):
+    for i in range(0, len(data), batch_size):
+        yield data[i:i + batch_size]
+
 async def main():
     root_dir = "subtitles"
     def resolve(item): return path.join(root_dir, item)
@@ -15,7 +20,7 @@ async def main():
 
     # 读取任务
     with open(resolve("task.txt"), "r", encoding="utf-8") as file:
-        filenames = [item for item in file.readlines() if len(item.strip()) > 0]
+        filenames = [item.strip() for item in file.readlines() if len(item.strip()) > 0]
     
     # 构建 chat_client
     api_key = args.api_key
@@ -23,15 +28,17 @@ async def main():
         api_key = os.environ.get('DEEPSEEK_API_KEY')
     chat_client = build_async_client(api_key=api_key)
 
-    # 异步执行
-    tasks = [
-        asyncio.create_task(txt_ai_translate_async(
-            txt_file=resolve(filename.strip()),
-            chat_client=chat_client,
-            topic="Power Platform: PL-100",
-        )) for filename in filenames
-    ]
-    await asyncio.gather(*tasks)
+    # 异步分批执行
+    for batch in batch_generator(filenames, 5):
+        print(f"exec batch: {",".join(batch)}")
+        tasks = [
+            asyncio.create_task(txt_ai_translate_async(
+                txt_file=resolve(filename),
+                chat_client=chat_client,
+                topic="Power Platform: PL-100",
+            )) for filename in batch
+        ]
+        await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
